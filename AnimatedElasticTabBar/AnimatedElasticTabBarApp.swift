24e045a76921f9eed21dcf4dfa62102182c61595
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-08-14 20:27:38 -0300

    Initial Commit

---
+++ AnimatedElasticTabBar/AnimatedElasticTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedElasticTabBarApp.swift
+//  AnimatedElasticTabBar
+//
+//  Created by Eduardo Santos on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedElasticTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
